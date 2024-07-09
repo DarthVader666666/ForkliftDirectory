e675a054e4a218c185d53aef051c357de0fa8470
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-07-09 20:09:11 +0300

    Initial Commit

---
+++ ForkliftDirectory.Server/Entities/Forklift.cs
@@ -0,0 +1,10 @@
+﻿namespace ForkliftDirectory.Server.Entities
+{
+    public class Forklift
+    {
+        public int? ForkliftId { get; set; }
+        public string? Model { get; set; }
+        public string? Number { get; set; }
+        public int? LiftCapacity { get; set; }
+    }
+}
---
+++ ForkliftDirectory.Server/ForkliftDirectoryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ForkliftDirectory.Server
+{
+    public class ForkliftDirectoryDbContext : DbContext
+    {
+        public ForkliftDirectoryDbContext(DbContextOptions<ForkliftDirectoryDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
